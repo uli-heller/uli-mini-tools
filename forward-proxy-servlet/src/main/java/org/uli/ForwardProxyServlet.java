c7d099de069dbdef2bfc4a061df8f76ceed8cd9e
Uli Heller <uli.heller@daemons-point.com>
2013-09-20 08:03:24 +0200

    Created eclipse project

---
+++ forward-proxy-servlet/src/main/java/org/uli/ForwardProxyServlet.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.uli;
+
+import org.eclipse.jetty.proxy.ProxyServlet;
+
+
+/**
+ * @author uli
+ *
+ */
+public class ForwardProxyServlet extends ProxyServlet {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 
+     */
+    public ForwardProxyServlet() {
+        super();
+    }
+}
