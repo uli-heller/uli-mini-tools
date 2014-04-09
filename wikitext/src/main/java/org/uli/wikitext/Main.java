bd4abf386804cefd08579eb96faa19f23714b3e6
Uli Heller <uli.heller@daemons-point.com>
2014-04-09 09:36:05 +0200

    Still: Work in progress...

--- wikitext/src/main/java/org/uli/wikitext/Main.java
+++ wikitext/src/main/java/org/uli/wikitext/Main.java
@@ -5,8 +5,11 @@ import java.lang.reflect.InvocationTargetException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
 
@@ -17,6 +20,7 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
+import org.eclipse.mylyn.wikitext.confluence.core.ConfluenceLanguage;
 import org.eclipse.mylyn.wikitext.core.parser.DocumentBuilder;
 import org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
 import org.eclipse.mylyn.wikitext.core.parser.builder.DocBookDocumentBuilder;
@@ -24,6 +28,11 @@ import org.eclipse.mylyn.wikitext.core.parser.builder.HtmlDocumentBuilder;
 import org.eclipse.mylyn.wikitext.core.parser.builder.XslfoDocumentBuilder;
 import org.eclipse.mylyn.wikitext.core.parser.markup.MarkupLanguage;
 import org.eclipse.mylyn.wikitext.core.util.ServiceLocator;
+import org.eclipse.mylyn.wikitext.markdown.core.MarkdownLanguage;
+import org.eclipse.mylyn.wikitext.mediawiki.core.MediaWikiLanguage;
+import org.eclipse.mylyn.wikitext.textile.core.TextileLanguage;
+import org.eclipse.mylyn.wikitext.tracwiki.core.TracWikiLanguage;
+import org.eclipse.mylyn.wikitext.twiki.core.TWikiLanguage;
 import org.uli.util.MyOptionBuilder;
 
 public class Main {
@@ -44,7 +53,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, IOException {
         int exitCode = run(args);
         if (exitCode != 0) {
             System.exit(exitCode);
@@ -59,7 +68,13 @@ public class Main {
         helpFormatter.printHelp(NAME, options);
     }
 
-    static public int run(String[] args) {
+    static public int run(String[] args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, IOException {
+        Reader r = new FileReader(args[0]);
+        Main main = new Main();
+        System.out.println(main.parse(extensionEnum.MARKDOWN, builderEnum.HTML, r));
+        if (1 == 1) {
+            return 0;
+        }
         Options options = new Options();
         Option h = MyOptionBuilder.init().withLongOpt("help").withDescription("print help").create("h");
         Option s = MyOptionBuilder.init().withLongOpt("symbolic").withDescription("create a symbolic link instead of a hard link").create("s");
@@ -107,32 +122,33 @@ public class Main {
         return exitCode;
     }
     
-    String toHtml(MarkupLanguage language, String wikiText) {
+    String parse(extensionEnum inputFormat, builderEnum outputFormat, Reader text) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, IOException {
         StringWriter writer = new StringWriter();
-        HtmlDocumentBuilder builder = new HtmlDocumentBuilder(writer);
+        DocumentBuilder builder = outputFormat.getBuilder(writer);
+        MarkupLanguage language = inputFormat.getMarkupLanguage();
         MarkupParser parser = new MarkupParser(language, builder);
-        parser.parse(wikiText);
+        parser.parse(text);
         return writer.toString();
     }
     
     enum extensionEnum {
-        TEXTILE("textile", "Textile"),
-        TRAC("trac", "TracWiki"),
-        MEDIAWIKI("mediawiki", "MediaWiki"),
-        CONFLUENCE("confluence", "Confluence"),
-        TWIKI("twiki", "TWiki"),
-        MARKDOWN("md", "Markdown");
+        TEXTILE("textile", new TextileLanguage()),
+        TRAC("trac", new TracWikiLanguage()),
+        MEDIAWIKI("mediawiki", new MediaWikiLanguage()),
+        CONFLUENCE("confluence", new ConfluenceLanguage()),
+        TWIKI("twiki", new TWikiLanguage()),
+        MARKDOWN("md", new MarkdownLanguage());
         
         String extension;
-        String languageName;
+        MarkupLanguage language;
         
-        extensionEnum(String extension, String languageName) {
+        extensionEnum(String extension, MarkupLanguage language) {
             this.extension = extension;
-            this.languageName = languageName;
+            this.language = language;
         }
         
         public MarkupLanguage getMarkupLanguage() {
-            return ServiceLocator.getInstance().getMarkupLanguage(languageName);
+            return language;
         }
     }
     
