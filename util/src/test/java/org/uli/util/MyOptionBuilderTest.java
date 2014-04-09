fc7bc7e4447b2b837c63274aa6bd2c1e47b28b5c
Uli Heller <uli.heller@daemons-point.com>
2014-04-09 08:40:12 +0200

    Fixed indentation

--- util/src/test/java/org/uli/util/MyOptionBuilderTest.java
+++ util/src/test/java/org/uli/util/MyOptionBuilderTest.java
@@ -5,29 +5,30 @@ import static org.junit.Assert.*;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
-//import org.apache.commons.cli.HelpFormatter;
+// import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
-//import org.apache.commons.cli.OptionBuilder;
+// import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
-//import org.apache.commons.cli.ParseException;
+// import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 
 public class MyOptionBuilderTest
 {
+
     @Test
     public void testNew() {
-	Option option = MyOptionBuilder.init().create("x");
+        Option option = MyOptionBuilder.init().create("x");
     }
 
     @Test
     public void testSimpleOption() throws Exception {
-	String[] args = new String[] { "-x" };
-	Option option = MyOptionBuilder.init().create("x");
-	Options options = new Options();
-	options.addOption(option);
-	CommandLineParser commandLineParser = new PosixParser();
-	CommandLine commandLine = commandLineParser.parse(options, args);
-	assertTrue(commandLine.hasOption("x"));
-	assertFalse(commandLine.hasOption("X"));
+        String[] args = new String[] { "-x" };
+        Option option = MyOptionBuilder.init().create("x");
+        Options options = new Options();
+        options.addOption(option);
+        CommandLineParser commandLineParser = new PosixParser();
+        CommandLine commandLine = commandLineParser.parse(options, args);
+        assertTrue(commandLine.hasOption("x"));
+        assertFalse(commandLine.hasOption("X"));
     }
 }
