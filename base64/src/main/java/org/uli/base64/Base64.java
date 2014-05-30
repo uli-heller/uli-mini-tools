20ae023cd7c10f5025eec4e79e37b3e5a3a1c72f
Uli Heller <uli.heller@daemons-point.com>
2014-05-30 11:16:58 +0200

    Fixed indentation

--- base64/src/main/java/org/uli/base64/Base64.java
+++ base64/src/main/java/org/uli/base64/Base64.java
@@ -8,13 +8,13 @@ import java.io.IOException;
 public class Base64 {
 
     public String base64(String input) throws IOException {
-	File f = new File(input);
-	long fl = f.length();
-	byte[] bytes = new byte[(int) fl];
-	InputStream is = new FileInputStream(f);
-	is.read(bytes);
-	is.close();
-	return org.apache.commons.codec.binary.Base64.encodeBase64String(bytes);
+        File f = new File(input);
+        long fl = f.length();
+        byte[] bytes = new byte[(int) fl];
+        InputStream is = new FileInputStream(f);
+        is.read(bytes);
+        is.close();
+        return org.apache.commons.codec.binary.Base64.encodeBase64String(bytes);
     }
 
     static public void main(String[] args) throws Exception {
