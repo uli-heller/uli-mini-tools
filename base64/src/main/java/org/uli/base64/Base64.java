11bf0db3e652b169f577fbf55e7b09498dcb170a
Uli Heller <uli.heller@daemons-point.com>
2014-05-30 11:26:06 +0200

    base64: Usage is now similar to md5sum

--- base64/src/main/java/org/uli/base64/Base64.java
+++ base64/src/main/java/org/uli/base64/Base64.java
@@ -1,26 +1,29 @@
 package org.uli.base64;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.io.IOException;
+
+import org.uli.util.FileToByteArray;
 
 public class Base64 {
 
-    public String base64(String input) throws IOException {
-        File f = new File(input);
-        long fl = f.length();
-        byte[] bytes = new byte[(int) fl];
-        InputStream is = new FileInputStream(f);
-        is.read(bytes);
-        is.close();
+    public String base64(byte[] bytes) throws IOException {
         return org.apache.commons.codec.binary.Base64.encodeBase64String(bytes);
     }
 
     static public void main(String[] args) throws Exception {
         Base64 base64 = new Base64();
-        for (String arg : args) {
-            System.out.println(base64.base64(arg));
+        if (args.length <= 0) {
+          FileToByteArray ftba = new FileToByteArray(System.in);
+          System.out.println(base64.base64(ftba.getBytes()));
+        } else {
+          boolean fPrintSuffix = false;
+          if (args.length > 1) {
+              fPrintSuffix=true;
+          }
+          for (String arg : args) {
+            FileToByteArray ftba = new FileToByteArray(arg);
+            System.out.println(base64.base64(ftba.getBytes())+(fPrintSuffix ? "  "+ftba.getFilename() : ""));
+          }
         }
     }
 }
