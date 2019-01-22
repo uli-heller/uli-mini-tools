9088a17ee0f6d80624e7999f3135a3d73e4e671a
Uli Heller <uli.heller@daemons-point.com>
2019-01-22 16:04:17 +0100

    http-cat: new option '-u username:password'

--- http-cat/src/main/java/org/uli/httpcat/HttpCat.java
+++ http-cat/src/main/java/org/uli/httpcat/HttpCat.java
@@ -2,18 +2,36 @@ package org.uli.httpcat;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class HttpCat {
     public static void main(String[] args) throws IOException {
-        if (args.length != 1) {
-            System.err.println("USAGE: httpcat url");
+        if (args.length != 1 && args.length != 3) {
+            System.err.println("USAGE: httpcat [-u username:password] url");
             System.exit(1);
         }
-        String url = args[0];
+	String url=args[0];
+	String name=null;
+	String value=null;
+	if (args.length == 3) {
+	    if (args[0].equals("-u")) {
+	      String userName = args[1];
+	      String encoded = Base64.encode(userName.getBytes());
+	      url=args[2];
+	      name="Authorization";
+	      value="Basic "+encoded;
+	    } else {
+              System.err.println("USAGE: httpcat [-u username:password] url");
+              System.exit(1);
+	    }
+	}
         URL u = new URL(url);
         URLConnection c = u.openConnection();
+	if (name != null && value != null) {
+	    c.setRequestProperty(name, value);
+	}
         InputStream is = c.getInputStream();
         byte[] buffer = new byte[10240];
         int len = 0;
@@ -21,5 +39,101 @@ public class HttpCat {
         while ((len = is.read(buffer)) >= 0) {
             System.out.write(buffer, 0, len);
         }
+    }
+}
+
+class Base64
+{
+    public static String encode(byte[] data)
+    {
+        char[] tbl = {
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+            'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
+            'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+            'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/' };
+
+        StringBuilder buffer = new StringBuilder();
+        int pad = 0;
+        for (int i = 0; i < data.length; i += 3) {
+
+            int b = ((data[i] & 0xFF) << 16) & 0xFFFFFF;
+            if (i + 1 < data.length) {
+                b |= (data[i+1] & 0xFF) << 8;
+            } else {
+                pad++;
+            }
+            if (i + 2 < data.length) {
+                b |= (data[i+2] & 0xFF);
+            } else {
+                pad++;
+            }
+
+            for (int j = 0; j < 4 - pad; j++) {
+                int c = (b & 0xFC0000) >> 18;
+                buffer.append(tbl[c]);
+                b <<= 6;
+            }
+        }
+        for (int j = 0; j < pad; j++) {
+            buffer.append("=");
+        }
+
+        return buffer.toString();
+    }
+
+    public static byte[] decode(String data)
+    {
+        int[] tbl = {
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54,
+            55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2,
+            3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30,
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+            48, 49, 50, 51, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+        byte[] bytes = data.getBytes();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        for (int i = 0; i < bytes.length; ) {
+            int b = 0;
+            if (tbl[bytes[i]] != -1) {
+                b = (tbl[bytes[i]] & 0xFF) << 18;
+            }
+            // skip unknown characters
+            else {
+                i++;
+                continue;
+            }
+
+            int num = 0;
+            if (i + 1 < bytes.length && tbl[bytes[i+1]] != -1) {
+                b = b | ((tbl[bytes[i+1]] & 0xFF) << 12);
+                num++;
+            }
+            if (i + 2 < bytes.length && tbl[bytes[i+2]] != -1) {
+                b = b | ((tbl[bytes[i+2]] & 0xFF) << 6);
+                num++;
+            }
+            if (i + 3 < bytes.length && tbl[bytes[i+3]] != -1) {
+                b = b | (tbl[bytes[i+3]] & 0xFF);
+                num++;
+            }
+
+            while (num > 0) {
+                int c = (b & 0xFF0000) >> 16;
+                buffer.write((char)c);
+                b <<= 8;
+                num--;
+            }
+            i += 4;
+        }
+        return buffer.toByteArray();
     }
 }
